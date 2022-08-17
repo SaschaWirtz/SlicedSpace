707ce48a9417a5772f6d2b6fe7fbf61048515194
Sascha Wirtz <58302057+TrickyTreat@users.noreply.github.com>
2022-08-17 22:43:45 +0200

    Soundslider in pause menu and fixed pause menu
    Added soundslider to pause menu and fixed pause menu

--- SlicedSpace/Assets/Scripts/PauseMenu.cs
+++ SlicedSpace/Assets/Scripts/PauseMenu.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -9,30 +11,77 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] GameObject pauseMenu;
 
     public static PauseMenu P1;
+    GameObject startButtonClicked;
+    GameObject exitButtonClicked;
+    GameObject startButton;
+    GameObject exitButton;
+    AudioSource[] sounds;
+    AudioSource popcornBag_crumble;
+    AudioSource doorOpen_exit;
+    bool closePauseMenu = false;
+    
+    void Start() {
+        this.sounds = GetComponents<AudioSource>();
+        this.popcornBag_crumble = this.sounds[0];
+        this.doorOpen_exit = this.sounds[1];
+
+        this.startButtonClicked = GameObject.Find("start-button-clicked");
+        this.exitButtonClicked = GameObject.Find("exit-button-clicked");
+        this.exitButton = GameObject.Find("exit-button");
+        this.startButton = GameObject.Find("start-button");
+        this.startButtonClicked.SetActive(false);
+        this.exitButtonClicked.SetActive(false);
+    }
 
     void Awake (){
         P1 = this;
         this.pauseMenu.SetActive(false);
         transform.Find("Resume").GetComponent<Button>().Select();
     }
+
+    void FixedUpdate() {
+        if(this.closePauseMenu) {
+            this.startButtonClicked.SetActive(false);
+            this.exitButtonClicked.SetActive(false);
+            this.startButton.SetActive(true);
+            this.exitButton.SetActive(true);
+            this.closePauseMenu = false;
+            pauseMenu.SetActive(false);
+        }
+    }
     
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;   
-        
     }
 
     public void Resume()
     {
-        pauseMenu.SetActive(false);
+        this.ResumeCo();
+    }
+
+    async void ResumeCo() {
+        this.popcornBag_crumble.Play();
+        GameObject.Find("start-button").SetActive(false);
+        this.startButtonClicked.SetActive(true);
+        await Task.Delay(700);
+        this.closePauseMenu = true;
         Time.timeScale = 1f;
     }
 
     public void Home(int sceneID)
     {
-            Time.timeScale = 1f;
-            SceneManager.LoadScene(sceneID); 
+        this.HomeCo(sceneID);
+    }
+
+    async void HomeCo(int sceneID) {
+        this.doorOpen_exit.Play();
+        GameObject.Find("exit-button").SetActive(false);
+        this.exitButtonClicked.SetActive(true);
+        await Task.Delay(700);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID); 
     }
    
 }
--- SlicedSpace/Assets/VolumeControls.cs
+++ SlicedSpace/Assets/VolumeControls.cs
@@ -2,10 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class VolumeControls : MonoBehaviour
 {
+    [SerializeField]
     public AudioMixer mixer;
+    public string outputName;
+
+    void Awake() {
+        this.mixer.GetFloat(this.outputName, out float value);
+        if(this.outputName == "SoundsVolume") {
+            GetComponent<Slider>().value = Mathf.Pow(10, value / 20);
+        }else if(this.outputName == "MusicVolume") {
+            GetComponent<Slider>().value = Mathf.Pow(10, (value + 2) / 20);
+        }
+        
+    }
 
     public void SetSoundsVolume(float sliderValue) {
         mixer.SetFloat("SoundsVolume", Mathf.Log10(sliderValue) * 20);
